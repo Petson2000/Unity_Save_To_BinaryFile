93bc260fd8dd553052eed18121d400782824f4c8
Petson2000 <erik.pettersson@futuregames.nu>
2020-03-04 15:21:10 +0100

    Basic Save to binary file system for a player character.

---
+++ SaveToBinaryFile/Assets/Script/PlayerData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData 
+{
+    public int level;
+    public int health;
+    public bool alive;
+    public string name;
+    public float[] position;
+
+    public PlayerData(Player player)
+    {
+        level = player.level;
+        health = player.health;
+
+        alive = player.alive;
+
+        name = player.name;
+
+        position = new float[3];
+
+        position[0] = player.transform.position.x;
+        position[1] = player.transform.position.y;
+        position[2] = player.transform.position.z;
+    }
+}
---
+++ SaveToBinaryFile/Assets/Script/SaveSystem.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+public static class SaveSystem
+{
+    public static void SavePlayer(Player player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter(); //Create binary formatter for changing data to binary
+
+        string path = Application.persistentDataPath + "/player.bin";
+
+        FileStream stream = new FileStream(path, FileMode.Create); //create file on system.
+
+        PlayerData data = new PlayerData(player); //runs cTor in PlayerData, saves data.
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static PlayerData LoadPlayer()
+    {
+        string path = Application.persistentDataPath + "/player.bin";
+        if(File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+           PlayerData data = (PlayerData)formatter.Deserialize(stream); //Change back from binary
+
+           stream.Close();
+
+           return data;
+        }
+
+        else
+        {
+            Debug.Log("Save file not found in path: " + path);
+            return null;
+        }
+    }
+}
